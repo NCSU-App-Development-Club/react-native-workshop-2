ef0e4dc706f3211c8d69d9bea1ea1dae5d7f3480
Abel Lu <alu4@ncsu.edu>
2024-09-30 19:39:31 -0400

    updates

--- App.tsx
+++ App.tsx
@@ -5,33 +5,33 @@ import { useState, useEffect } from 'react';
 
 export default function App() {
 
-  const [topItemids, setTopItemIds] = useState([]);
-  const [error, setError] = useState(false);
-  const [limit, setLimit] = useState(10);
+  const [topStoryIds, setTopStoryIds] = useState<number[]>([]);
+  const [error, setError] = useState<boolean>(false);
+  const limit = 10;
 
-  async function getTopItemIds() {
+  async function getTopStoryIds() {
     setError(false);
     try {
       const res = await fetch("https://hacker-news.firebaseio.com/v0/topstories.json?print=pretty");
       const data = await res.json();
-      setTopItemIds(data);
+      setTopStoryIds(data);
     } catch {
       setError(true);
     }
   }
 
   useEffect(() => {
-    getTopItemIds();
+    getTopStoryIds();
   }, [])
 
   return (
     <View style={styles.container}>
       {
-        topItemids.slice(0, limit).map((itemId) => {
-          return <Text key={itemId}>{itemId}</Text>;
+        topStoryIds.slice(0, limit).map((storyId) => {
+          return <Text key={storyId}>{storyId}</Text>;
         })
       }
-      <Text>{error && "Error fetching top items."}</Text>
+      <Text key="error">{error && "Error fetching top items."}</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -42,7 +42,6 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
-    borderWidth: 8,
     height: Platform.OS == "android" ? Dimensions.get('window').height : Dimensions.get('window').height - Constants.statusBarHeight,
     width: Dimensions.get('window').width,
     top: Constants.statusBarHeight,
